3585d8a39d35c44e62c4b155b8dd242cbab044a1
AureusOnly <159770929+AureusOnly@users.noreply.github.com>
2024-03-04 11:04:27 +0100

    MAJ

--- list.php
+++ list.php
@@ -3,14 +3,33 @@
 -->
 <?php
 require_once("head.php");
+?>
+<table>
+    <thead>
+        <th>
+            nom Pokemon
+        </th>
+        <th>
+            Image
+        </th>
+        <th>Type 1</th>
+        <th>Type 2</th>
+    </thead>
+</table>
+<?php
 require_once("database-connection.php")
 $query = $databaseConnection->query("SELECT nomPokemon, urlPhoto, T.libelleType AS 'Type 1',T2.LibelleType AS 'Type 2'
     FROM Pokemon P
     JOIN TypePokemon T ON P.idTypePokemon=T.idType
     LEFT JOIN TypePokemon T2 ON P;idSecondTypePokemon=T2.idtype
+    ORDER BY idPokemon ASC
 ");
 $result=$query->fetch_all(MYSQLI_ASSOC);
-var_dump($result)
+var_dump($result);
+foreach($result as $pokemon) {
+$row
+echo $row
+};
 ?>
 
 
